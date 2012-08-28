9128b75996f93607c3b39d3e4202a1e09a676176
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-08-28 10:05:34 -0600

    updated footer

--- Code_Igniter/application/views/elements/footer++.php
+++ Code_Igniter/application/views/elements/footer++.php
@@ -3,7 +3,7 @@
 ?>
 
 		<footer class="center ui-widget">
-			&copy; 2012 <a href="http://www.aidwebsolutions.com" title="go to Aid Web Solutions site in a new tab" target="_blank">Aid Web Solutions</a>
+			Copyright &copy; 2012 <a href="http://www.aidwebsolutions.com" title="go to Aid Web Solutions site in a new tab" target="_blank">Martijn van de Rijdt</a>
 		</footer>
 		
 	</body>

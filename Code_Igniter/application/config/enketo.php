088e4f0f0a01976d4156a31503201100df3f9b32
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-09-11 12:19:05 -0600

    new configuration file

---
+++ Code_Igniter/application/config/enketo.php
@@ -0,0 +1,35 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Configuration file for the application cache by Martijn van de Rijdt
+*/
+
+/*
+|--------------------------------------------------------------------------
+| 
+|--------------------------------------------------------------------------
+|
+| |
+*/
+
+// centrally disable application cache for all pages (subdomains)
+$config['application_cache'] = FALSE;
+
+//$config['survey_offline'] = array(
+//	'js'=>array(
+//		
+//	),
+//	'css' => array(
+//	
+//	
+//	),
+//	'other' => array(
+//	
+//	)
+//);
+//
+//
+
+$config['google_maps_api_v3_key'] = "AIzaSyDF5xYZfxN7r5SsNPGstjAeTzwa6dVU4Ik";
+
+

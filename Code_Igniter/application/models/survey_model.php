dafd2d2e03174614b1826a7610c38edcd46ed646
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-10-04 11:50:36 -0600

    Revert "Revert "change online-only subdomain suffix from '-' to '-0' to try to solve a possible issue with some DNS servers""
    This reverts commit 99ed57b9f5503f49a598a160525695d621f658d8.

--- Code_Igniter/application/models/survey_model.php
+++ Code_Igniter/application/models/survey_model.php
@@ -26,14 +26,13 @@ class Survey_model extends CI_Model {
         log_message('debug', 'Survey Model loaded');
         $this->load->helper(array('subdomain', 'url', 'string', 'http'));
     	$this->subdomain = get_subdomain();
-        $this->ONLINE_SUBDOMAIN_SUFFIX = '-';
+        $this->ONLINE_SUBDOMAIN_SUFFIX = '-0';
         date_default_timezone_set('UTC');
     }
     
     // returns true if a requested survey form is live / published, used for manifest
     public function is_live_survey($survey_subdomain = NULL)
     {
-
     	//$query = $this->db->get_where('surveys',array('subdomain'=>$survey_subdomain, 'survey_live'=>TRUE), 1);
     	//return ($query->num_rows() === 1) ? TRUE : FALSE;
         //since management of surveys is done at the OpenRosa-compliant server, all we could do here is see
@@ -242,7 +241,7 @@ class Survey_model extends CI_Model {
     private function _get_item($field)
     {
         $subd = $this->subdomain;
-        $db_subd = ( $this->_has_subdomain_suffix() ) ? substr($subd, 0, strlen($subd)-1) : $subd;
+        $db_subd = ( $this->_has_subdomain_suffix() ) ? substr($subd, 0, strlen($subd)-strlen($this->ONLINE_SUBDOMAIN_SUFFIX)) : $subd;
         $this->db->select($field);
         $this->db->where('subdomain', $db_subd); //$this->subdomain);
         $query = $this->db->get('surveys', 1); 
@@ -276,7 +275,7 @@ class Survey_model extends CI_Model {
     private function _has_subdomain_suffix()
     {
         $s = $this->subdomain;
-        return ( substr($s, strlen($s)-1) === $this->ONLINE_SUBDOMAIN_SUFFIX ) ? TRUE : FALSE; 
+        return ( substr($s, strlen($s)-strlen($this->ONLINE_SUBDOMAIN_SUFFIX)) === $this->ONLINE_SUBDOMAIN_SUFFIX ) ? TRUE : FALSE; 
     }
 //    //returns the form format as an object   NOT USED ANYMORE! 
 //    public function get_form_format($id = NULL, $live = TRUE)

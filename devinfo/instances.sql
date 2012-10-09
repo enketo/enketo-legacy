ee945e14301a8ec2a9e8f3657dbd3a07c8cdb2b5
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-10-09 09:12:22 -0600

    updated documentation

--- devinfo/instances.sql
+++ devinfo/instances.sql
@@ -1,12 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 08, 2012 at 08:36 PM
+-- Server version: 5.5.24
+-- PHP Version: 5.3.10-1ubuntu3.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `enketo`
+--
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `instances`
 --
 
-CREATE TABLE `instances` (
-    `instanceid` varchar(255) NOT NULL,
-    `subdomain` varchar(100) NOT NULL,
-    `return_url` varchar(255) NOT NULL,
-    `instance_xml` mediumtext,
-    UNIQUE KEY `idx_instanceid` (`instanceid`),
-    KEY `idx_subdomain` (`subdomain`)
+CREATE TABLE IF NOT EXISTS `instances` (
+  `instanceid` varchar(255) NOT NULL,
+  `subdomain` varchar(100) NOT NULL,
+  `return_url` varchar(255) NOT NULL,
+  `instance_xml` mediumtext,
+  `timestamp` bigint(20) NOT NULL,
+  UNIQUE KEY `idx_instanceid` (`instanceid`),
+  KEY `idx_subdomain` (`subdomain`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

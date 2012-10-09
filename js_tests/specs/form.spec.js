2ad03dca2c3c334cbbe23b48dfe08c39db22a770
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-10-09 16:42:40 -0600

    added separate Jasmine 1.2.0 library for browser tests (the one in EnvJasmine is older and less nice)

--- js_tests/specs/form.spec.js
+++ js_tests/specs/form.spec.js
@@ -4,8 +4,8 @@ EnvJasmine.load(EnvJasmine.mocksDir + "form.mock.js");
 EnvJasmine.load(EnvJasmine.includeDir + "xpathjs_javarosa/build/xpathjs_javarosa.min.js");
 EnvJasmine.load(EnvJasmine.jsDir + "__form.js");
 
-describe("get XML Node", function () {
-    it("obtains XML Node(s) from instance", function() {
+describe("Data node getter function", function () {
+    it("obtains XML node(s) from instance", function() {
         var t =
 			[
 				["", null, null, 9],
@@ -42,36 +42,62 @@ describe("get XML Node", function () {
 			],
 			form = new Form("", ""),
 			data = form.data(dataStr1);
-		//beforeEach(function(){});
 		
 		for (i = 0 ; i<t.length ; i++){
 			expect(data.node(t[i][0], t[i][1], t[i][2]).get().length).toEqual(t[i][3]);
-			//'selector: '+t[i][0]+'   index: '+t[i][1]+',   filter: '+JSON.stringify(t[i][2]));
 		}
     });
 });
 
-describe('set XML node value if passing data type conversion and validation', function(){
+describe('Data node XML data type validation', function(){
+	var data,
+		form = new Form("", "");
+	form.form('<form></form>');
+
 	it('validates xml-type (without constraint evaluation)', function(){
 		var t =
 			[
 				["/thedata/nodeA", null, null, 'val1', null, true],
-				["/thedata/nodeA", null, null, 'val2', 'string', true],
 				["/thedata/nodeA", null, null, 'val3', 'somewrongtype', true],
-				["/thedata/nodeA", null, null, 'val4', 'int', false],
-				["/thedata/nodeA", null, null, 'val5565ghgyuyuy', 'date', false], //Chrome turns val5 into a valid date...
-				["/thedata/nodeA", null, null, 'val5565ghgyuyua', 'datetime', false], //Chrome turns val10 into a valid date..
-				["/thedata/nodeA", null, null, 'val11', 'decimal', false],
-				["/thedata/nodeA", 0   , null, 'val12', 'string', true],
+
 				["/thedata/nodeA", 1   , null, 'val13', 'string', null], //non-existing node
-				["/thedata/nodeA", 0   , null, '14', 'string', true],
-				["/thedata/nodeA", 0   , null, 1, 'string', true],
-				["/thedata/nodeA", 0   , null, '2', 'int', true],
-				["/thedata/nodeA", 0   , null, 3, 'int', true],
-				["/thedata/nodeA", 0   , null, '4', 'double', true],
+				["/thedata/repeatGroup/nodeC", null, null, 'val', null], //multiple nodes
+
+				["/thedata/nodeA", 0   , null, '4', 'double', true], //non-existing xml data type -> string
 				["/thedata/nodeA", 0   , null, 5, 'double', true],
+
+				["/thedata/nodeA", null, null, 'val2', 'string', true],
 				["/thedata/nodeA", 0   , null, ['a', 'b', 'c'], 'string', true],
 				["/thedata/nodeA", 0   , null, ['d', 'e', 'f', ''], 'string', true],
+				["/thedata/nodeA", 0   , null, 'val12', 'string', true],
+				["/thedata/nodeA", 0   , null, '14', 'string', true],
+				["/thedata/nodeA", 0   , null, 1, 'string', true],
+				
+				["/thedata/nodeA", null, null, 'val11', 'decimal', false],
+
+				["/thedata/nodeA", null, null, 'val4', 'int', false],
+				["/thedata/nodeA", 0   , null, '2', 'int', true],
+				["/thedata/nodeA", 0   , null, 3, 'int', true],
+
+				["/thedata/nodeA", null, null, 'val5565ghgyuyuy', 'date', false], //Chrome turns val5 into a valid date...
+/*Rhino*/		["/thedata/nodeA", null, null, '2012-01-01', 'date', true],
+				["/thedata/nodeA", null, null, '2012-12-32', 'date', false],
+				["/thedata/nodeA", null, null, 324, 'date', true],
+				
+				["/thedata/nodeA", null, null, 'val5565ghgyuyua', 'datetime', false], //Chrome turns val10 into a valid date..
+				["/thedata/nodeA", null, null, '2012-01-01T00:00:00-06', 'datetime', true],
+				["/thedata/nodeA", null, null, '2012-12-32T23:59:59-06', 'datetime', true],
+				["/thedata/nodeA", null, null, '2012-01-01T30:00:00-06', 'datetime', true], //datetimes can deal with 25:00s
+
+				["/thedata/nodeA", null, null, 'a', 'time', false],
+				["/thedata/nodeA", null, null, 'aa:bb', 'time', false],
+/*FF,Rhino*/	["/thedata/nodeA", null, null, '0:0', 'time', true],
+/*FF,Rhino*/	["/thedata/nodeA", null, null, '00:00', 'time', true],
+/*FF,Rhino*/	["/thedata/nodeA", null, null, '23:59', 'time', true],
+				["/thedata/nodeA", null, null, '24:00', 'time', false],
+
+				["/thedata/nodeA", null, null, 'val2', 'barcode', true],
+
 				["/thedata/nodeA", null, null, '0 0 0 0', 'geopoint', true],
 				["/thedata/nodeA", null, null, '10 10', 'geopoint', true],
 				["/thedata/nodeA", null, null, '10 10 10', 'geopoint', true],
@@ -85,38 +111,41 @@ describe('set XML node value if passing data type conversion and validation', fu
 				["/thedata/nodeA", null, null, '0 a', 'geopoint', false],
 				["/thedata/nodeA", null, null, '0', 'geopoint', false],
 				["/thedata/nodeA", null, null, '0 0 a', 'geopoint', false],
-				["/thedata/nodeA", null, null, '0 0 0 a', 'geopoint', false],
+				["/thedata/nodeA", null, null, '0 0 0 a', 'geopoint', false]
 
-				//TODO Other Data Types
-				
-				["/thedata/repeatGroup/nodeC", null, null, 'val', null] //multiple
-			],
-			form = new Form("", ""),
-			data;
-		form.form('<form></form>');
-
-		beforeEach(function(){
-			
-		});
+				//TO DO binary (?)
+			];
 
 		for (i = 0 ; i<t.length ; i++){
-			//data should be reset in a beforeEach function (once I have access to the jasmine documentation)
 			data = form.data(dataStr1);
 			expect(data.node(t[i][0], t[i][1], t[i][2]).setVal(t[i][3], null, t[i][4])).toEqual(t[i][5]);
 		}
+	});
 
+	it('sets a non-empty value to empty', function(){
+		var node = data.node('/thedata/nodeA', null, null);
+		data = form.data(dataStr1);
+		node.setVal('value', 'string');
+		expect(node.setVal(''), true);
 	});
 });
 
-//validation of empty values (so done after first setting a non-empty value in the target node)
-//
-//["/thedata/nodeA", 0   , null, [], 'string', true], 
-//
-//
-//
-//
+describe("Data node remove function", function(){
+	it("has removed a data node", function(){
+		var form = new Form('', ''),
+			data = form.data(dataStr1),
+			node = data.node("/thedata/nodeA");
+		form.form('form');
 
-describe("evaluate XPath", function(){
+		expect(node.get().length).toEqual(1);
+		data.node("/thedata/nodeA").remove();
+		expect(node.get().length).toEqual(0);
+	});
+
+});
+
+
+describe("XPath Evaluator", function(){
 	it('evaluates an XPath expression', function(){
 		var t =
 		[

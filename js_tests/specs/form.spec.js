7ff14e70ba0967bed3715ae5d56aec4d39777530
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-10-18 16:10:04 -0600

    more grammar corrections

--- js_tests/specs/form.spec.js
+++ js_tests/specs/form.spec.js
@@ -160,7 +160,7 @@ describe('Data node XML data type conversion & validation', function(){
 	});
 });
 
-describe("clones a data node", function(){
+describe("Data node cloner", function(){
 	it("has cloned a data node", function(){
 		var form = new Form('', ''),
 			data = form.data(dataStr1),
@@ -174,7 +174,7 @@ describe("clones a data node", function(){
 	});
 });
 
-describe("removes a data node", function(){
+describe("Data node remover", function(){
 	it("has removed a data node", function(){
 		var form = new Form('', ''),
 			data = form.data(dataStr1),
@@ -224,7 +224,7 @@ describe("XPath Evaluator (see github.com/MartijnR/xpathjs_javarosa for comprehe
 	});
 });
 
-describe("Output test", function(){
+describe("Output functionality ", function(){
 	// These tests were orginally meant for modilabs/enketo issue #141. However, they passed when they were
 	// failing in the enketo client itself (same form). It appeared the issue was untestable (except manually)
 	// since the issue was resolved by updating outputs with a one millisecond delay (!).
@@ -233,12 +233,12 @@ describe("Output test", function(){
 	
 	form.init();
 	
-	it("tests output upon initialization: node random__", function(){
+	it("tested upon initialization: node random__", function(){
 		console.debug(form.getFormO().$.find('[data-value="/random/random__"]'));
 		expect(form.getFormO().$.find('[data-value="/random/random__"]').text().length).toEqual(17);
 	});
 
-	it("tests output upon initialization: node uuid__", function(){
+	it("tested upon initialization: node uuid__", function(){
 		console.debug(form.getFormO().$.find('[data-value="/random/uuid__"]'));
 		expect(form.getFormO().$.find('[data-value="/random/uuid__"]').text().length).toEqual(36);
 	});

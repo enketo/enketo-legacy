940663e79c6e517586b82c5b427e76dd305d30e0
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-10-25 11:09:52 -0600

    add time logging between start and receiving response from OpenRosa server

--- Code_Igniter/application/controllers/data.php
+++ Code_Igniter/application/controllers/data.php
@@ -114,7 +114,7 @@ class Data extends CI_Controller {
 		log_message('debug', 'data submission from start to sending to OpenRosa server took '.(time()-$time_start).' seconds.');
 		//execute post
 		$result = curl_exec($ch);
-		
+		log_message('debug', 'data submission from start to receiving response from OpenRosa server took '.(time()-$time_start).' seconds.');
 		if (curl_errno($ch))
 		{
 			return 'curl_error:'.curl_error($ch).' number: '+curl_errno();

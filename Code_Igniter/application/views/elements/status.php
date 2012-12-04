5c508a9d4a7bea67a223dd7876e4de1964a6e506
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-12-04 16:15:38 -0700

    reorganize web form view

---
+++ Code_Igniter/application/views/elements/status.php
@@ -0,0 +1,8 @@
+<div class="drawer left closed alert-success">
+	<div class="handle right"></div>
+	<div class="content">
+		<span id="status"></span><span>Submissions queued: </span><span id="queue-length">0</span>
+		<!--<span id="status-offline-launch"></span>-->
+		<button id="drawer-export" class="btn btn-mini">Export</button>
+	</div>	
+</div>
--- Code_Igniter/application/views/webform_view.php
+++ Code_Igniter/application/views/webform_view.php
@@ -1,15 +1,22 @@
 <? 	
 	require 'elements/html_start.php'; 
-	$this->load->helper('subdomain'); 
 ?>
 		<script type="text/javascript">
 			var jrDataStr = '<?= $form_data ?>';
 			var supportEmail = '<?= $this->config->item("support_email") ?>';
 		</script>
 	
+	
+
+
 	</head>
 	<body>
 	<? if (ENVIRONMENT === 'production'){include_once 'elements/tracking.php';}?>
+
+		
+
+
+
 
 		<header style="height: 0;"></header>
 		
@@ -22,16 +29,10 @@
 			</div>
 		</article>
 
-		<div class="drawer left closed alert-success">
-			<div class="handle right"></div>
-			<div class="content">
-				<span id="status"></span><span>Submissions queued: </span><span id="queue-length">0</span>
-				<!--<span id="status-offline-launch"></span>-->
-				<button id="drawer-export" class="btn btn-mini">Export</button>
-			</div>	
-		</div>
+		<? require 'elements/status.php'; ?>	
 
 		<div id="branding">
-			<a href="http://aidwebsolutions.com" target="_blank">enketo forms</a> for <a href="http://formhub.org" target="_blank">formhub</a></div>
+			<a href="http://aidwebsolutions.com" target="_blank">enketo forms</a> for <a href="http://formhub.org" target="_blank">formhub</a>
+		</div>
 
 	<? require 'elements/footer++.php' ?>

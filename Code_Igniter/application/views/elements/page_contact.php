acea622a06f45b6e282d017add4f7e3140467f14
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-12-07 17:47:06 -0700

    remove title from contact page

--- Code_Igniter/application/views/elements/page_contact.php
+++ Code_Igniter/application/views/elements/page_contact.php
@@ -1,4 +1,4 @@
-<article id="contact" data-title="provide feedback or ask a question" class="page">				
+<article id="contact" class="page">				
 	<p>
 		Please write <a href="mailto:<?= $this->config->item('support_email') ?>"><?= $this->config->item('support_email') ?></a> for any comments, bug reports, 
 		questions, issues, as well as expressions of excitement or disappointment. 

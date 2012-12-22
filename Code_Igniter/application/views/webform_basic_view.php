9431959d4af3e252173c4742273ff5c186100867
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-12-21 18:22:04 -0700

    started with new form branding bar + print button

--- Code_Igniter/application/views/webform_basic_view.php
+++ Code_Igniter/application/views/webform_basic_view.php
@@ -20,16 +20,16 @@
 
 		<div class="main">
 			<article class="paper" >
+				<div class="form-header">
+					<span class="branding">enketo forms + formhub</span>
+					<button onclick="printO.printForm(); return false" class="print"><i class="icon-print"></i></button>
+				</div>
 				<?= $form ?>
 				<button id="submit-edited-data" class="btn btn-primary btn-large" ><i class="icon-ok icon-white"></i> Submit</button>
 			</div>
 		</article>
 
 		
-
-		<div id="branding">
-			<a href="http://aidwebsolutions.com" target="_blank">enketo forms</a> for <a href="http://formhub.org" target="_blank">formhub</a>
-		</div>
 
 	<? require 'elements/footer++.php' ?>
 

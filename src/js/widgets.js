c1ec303060abe7923f0eb69a31ed939007156ff3
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-12-28 15:21:18 -0700

    little cleanup + recompiled js

--- src/js/widgets.js
+++ src/js/widgets.js
@@ -217,7 +217,6 @@
 
         this.$lat = this.$widget.find('[name="lat"]');
         this.$lng = this.$widget.find('[name="long"]');
-         console.debug('this.$lng: ', this.$lng);
         this.$alt = this.$widget.find('[name="alt"]');
         this.$acc = this.$widget.find('[name="acc"]');
         
@@ -293,7 +292,6 @@
                     }, 500);   
                 });
             }
-            console.log('this in init:', this);
         },
         /**
          * Enables geo detection using the built-in browser geoLocation functionality

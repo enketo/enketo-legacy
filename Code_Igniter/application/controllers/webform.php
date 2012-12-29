6ad558fd5d5d6a635e2b10ec5c62451662472934
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2012-12-28 17:13:17 -0700

    clean up and update libraries

--- Code_Igniter/application/controllers/webform.php
+++ Code_Igniter/application/controllers/webform.php
@@ -31,11 +31,9 @@ class Webform extends CI_Controller {
 			'/libraries/bootstrap/js/bootstrap.min.js',	
 			'/libraries/jdewit-bootstrap-timepicker/js/bootstrap-timepicker.js',
 			'/libraries/bootstrap-datepicker/js/bootstrap-datepicker.js',
-			//'/libraries/bootstrap-select.js',
 			'/libraries/modernizr.min.js',
 			'/libraries/xpathjs_javarosa/build/xpathjs_javarosa.min.js',
-			'/libraries/vkbeautify.js'//,
-			//"http://maps.googleapis.com/maps/api/js?v=3.exp&key=".$this->config->item('google_maps_api_v3_key')."&sensor=false&libraries=places"
+			'/libraries/vkbeautify.js'
 		);
 		$this->default_stylesheets = array
 		(

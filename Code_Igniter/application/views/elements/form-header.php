b50a4dbec93c17821d29a8b96c8be791c57b29be
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2013-02-02 19:36:53 -0700

    fixed bug in log link on webforms

--- Code_Igniter/application/views/elements/form-header.php
+++ Code_Igniter/application/views/elements/form-header.php
@@ -1,6 +1,11 @@
 <div class="form-header clearfix">
-	<? 	$this->load->helper('subdomain');
-		$link = (!$integrated) ? str_replace(get_subdomain().'.', '', full_base_url()) : (empty($return_url)) ? $this->config->item('integration_with_url') : $return_url; ?>
+	<? 	
+		$this->load->helper('subdomain');
+		$subd = get_subdomain();
+		$subdot = (!empty($subd)) ? $subd.'.' : '';
+		$link = (!$integrated) ? str_replace($subdot, '', full_base_url()) : 
+			((empty($return_url)) ? $this->config->item('integration_with_url') : $return_url );
+	?>
 	<a class="branding" href="<?= $link ?>" title="Go Back"><?= $brand ?></a>
 	<span class="records" title="Records Queued - Click to Backup to File"><span class="queue-length"></span></span>
 	<button onclick="return false;" class="print"><img src="/images/print.png" alt="print button" title="Print Form"/></button>

e46cbc111a15bd4235511f00a44bb06ccefacac2
Martijn van de Rijdt <martijn@aidwebsolutions.com>
2013-04-18 09:24:55 -0600

    remove headers

--- Code_Igniter/application/views/webform_basic_view.php
+++ Code_Igniter/application/views/webform_basic_view.php
@@ -11,8 +11,6 @@
 	
 	</head>
 	<body>
-		
-		<header class="navbar navbar-inverse navbar-fixed-top" style="height: 0; display: none;"></header>
 			
 	<? require 'elements/dialogs.php'; ?>
 
--- Code_Igniter/application/views/webform_preview_view.php
+++ Code_Igniter/application/views/webform_preview_view.php
@@ -8,8 +8,6 @@
 		
 	</head>
 	<body>
-		
-		<header class="navbar navbar-inverse navbar-fixed-top" style="height: 0; display: none;"></header>
 			
 	<? require 'elements/dialogs.php'; ?>
 		<h3 class="preview">Form Preview</h3>

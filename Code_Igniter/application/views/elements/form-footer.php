3008ef394679cd5f659481902370ceacbd29b215
Martijn van de Rijdt <martijn@enketo.org>
2014-01-06 13:08:58 -0700

    fix style of mark-as-draft button

--- Code_Igniter/application/views/elements/form-footer.php
+++ Code_Igniter/application/views/elements/form-footer.php
@@ -1,6 +1,6 @@
 	<section class="form-footer">
 		<? if ($title_component === 'webform'): ?>
-		<label class="draft question"><span class="option-label">this is a draft</span><input class="ignore" type="checkbox" name="draft"/></label>
+		<div class="option-wrapper"><label class="draft question"><span class="option-label">this is a draft</span><input class="ignore" type="checkbox" name="draft"/></label></div>
 		<? endif; ?>
 		<button id="<?= ($title_component === 'webform single' || $title_component === 'webform edit') ? 'submit-form-single' : 'submit-form' ; ?>" class="btn btn-primary btn-large" ><i class="glyphicon glyphicon-ok"></i> Submit</button>
 		<? include_once 'enketo-power.php'; ?>

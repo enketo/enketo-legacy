5917ec7d0688ddcbefe1fce3b771e0a5749e5e99
Martijn van de Rijdt <martijn@enketo.org>
2014-01-10 12:30:34 -0700

    fixes single-submit view

--- Code_Igniter/application/views/elements/form-footer.php
+++ Code_Igniter/application/views/elements/form-footer.php
@@ -2,7 +2,7 @@
 		<? if ($title_component === 'webform'): ?>
 		<div class="option-wrapper"><label class="draft question"><span class="option-label">Save as Draft</span><input class="ignore" type="checkbox" name="draft"/></label></div>
 		<? endif; ?>
-		<button id="<?= ($title_component === 'webform single' || $title_component === 'webform edit') ? 'submit-form-single' : 'submit-form' ; ?>" class="btn btn-primary btn-large" ><i class="glyphicon glyphicon-ok"></i> Submit</button>
+		<button id="<?= ($title_component === 'webform single-submit' || $title_component === 'webform edit') ? 'submit-form-single' : 'submit-form' ; ?>" class="btn btn-primary btn-large" ><i class="glyphicon glyphicon-ok"></i> Submit</button>
 		<? include_once 'enketo-power.php'; ?>
 		<? include 'logout.php'; ?>
 	</section>

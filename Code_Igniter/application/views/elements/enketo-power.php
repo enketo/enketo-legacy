d935fb033f57d3c6f982253e321d268be92b7d14
Martijn van de Rijdt <martijn@enketo.org>
2014-01-20 12:46:15 -0700

    show powered by enketo when org own logo is used, #269

--- Code_Igniter/application/views/elements/enketo-power.php
+++ Code_Igniter/application/views/elements/enketo-power.php
@@ -1,5 +1,5 @@
 <?// quick and dirty check for whether logo is an enketo logo...?>
-<? if(empty($logo_url) || strpos($logo_url, 'enketo') === FALSE): ?>
+<? if(empty($logo_url) || strpos($logo_url, 'enketo_bare') === FALSE): ?>
 	<div class="enketo-power">Powered by <a href="http://enketo.org" title="enketo.org website">
 		<img src="/images/enketo_bare_100x37.png" alt="enketo logo" /></a>
 	</div>

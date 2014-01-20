f16c6b8b106840e2e13243b15f534e7f8afa5887
Martijn van de Rijdt <martijn@enketo.org>
2014-01-20 12:49:14 -0700

    form footer fixes, now shows logout link and responds nicely when ‘powered by enketo’ or logout link is not present.

--- Code_Igniter/application/views/elements/form-footer.php
+++ Code_Igniter/application/views/elements/form-footer.php
@@ -1,9 +1,11 @@
 	<section class="form-footer">
-		<? if ($title_component === 'webform'): ?>
-		<div class="option-wrapper"><label class="draft question"><span class="option-label">Save as Draft</span><input class="ignore" type="checkbox" name="draft"/></label></div>
-		<? endif; ?>
-		<button id="<?= ($title_component === 'webform single-submit' || $title_component === 'webform edit') ? 'submit-form-single' : 'submit-form' ; ?>" class="btn btn-primary btn-large" ><i class="glyphicon glyphicon-ok"></i> Submit</button>
-		<? include_once 'enketo-power.php'; ?>
-		<? include 'logout.php'; ?>
+		<div class="content">
+			<? if ($title_component === 'webform'): ?>
+			<div class="option-wrapper"><label class="draft question"><span class="option-label">Save as Draft</span><input class="ignore" type="checkbox" name="draft"/></label></div>
+			<? endif; ?>
+			<button id="<?= ($title_component === 'webform single-submit' || $title_component === 'webform edit') ? 'submit-form-single' : 'submit-form' ; ?>" class="btn btn-primary btn-large" ><i class="glyphicon glyphicon-ok"></i> Submit</button>
+			<? include 'logout.php'; ?>
+			<? include_once 'enketo-power.php'; ?>
+		</div>
 	</section>
 

e331924b5d8f61075f903527b3dafdae8e38eac2
Martijn van de Rijdt <martijn@enketo.org>
2014-04-29 08:49:50 -0600

    force appCache update to make sure last weeks fixes propagate properly on Chrome

--- Code_Igniter/application/controllers/manifest.php
+++ Code_Igniter/application/controllers/manifest.php
@@ -51,7 +51,7 @@ class Manifest extends CI_Controller {
     | force cache update 
     |--------------------------------------------------------------------------
     */
-        private $hash_manual_override = '0038'; //time();
+        private $hash_manual_override = '0039'; //time();
     /*
     |-------------------------------------------------------------------------- 
     | pages to be cached (urls relative to sub.example.com/)
